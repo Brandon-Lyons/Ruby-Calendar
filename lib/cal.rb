407f0952b7e5be63125180baa501cda4da80ff30
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-04-29 14:41:41 -0500

    more unit tests and code

--- lib/cal.rb
+++ lib/cal.rb
@@ -1,11 +1,13 @@
 class Calendar
 	attr_accessor :month
 	attr_accessor :year
+	attr_accessor :days
 	attr_accessor :first_day
 	attr_accessor :output
 
 	# month = ARGV[0]
 	# year = ARGV[1]
+
 
 	def initialize (month, year)
 		@month = month
@@ -14,7 +16,7 @@ class Calendar
 
 	def header
 		month_name = { 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December" }
-		@output = "#{month_name[@month]} #{year}\n"
+		@output = "#{month_name[@month]} #{year}".center(20) << "\n"
 	end
 
 	def week_row
@@ -35,6 +37,18 @@ class Calendar
 				
 		@first_day = (1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7
 		@first_day
+	end
+
+	def days_number
+		num_days = { 1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31 }
+		@days = num_days[@month]
+		if @year % 100 == 0 && @year % 400 == 0
+			num_days[2] = 29
+		elsif @year % 4 == 0 && @year % 100 != 0
+			num_days[2] = 29
+		end					
+		@days = num_days[@month]
+		@days
 	end
 
 
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -4,13 +4,13 @@ require 'cal'
 class CalTests < Test::Unit::TestCase
 
 	def test_header
-		cal = Calendar.new(2, 2012)
-		assert_equal("February 2012\n", cal.header)
+		cal = Calendar.new(5, 2012)
+		assert_equal("      May 2012      \n", cal.header)
 	end
 
 	def test_header_different_month
 		cal = Calendar.new(5, 1896)
-		assert_equal("May 1896\n", cal.header)
+		assert_equal("      May 1896      \n", cal.header)
 	end
 
 	# def test_no_month
@@ -21,8 +21,8 @@ class CalTests < Test::Unit::TestCase
 	# end
 
 	def test_week_row
-		cal = Calendar.new(2, 2012)
-		assert_equal("February 2012\nSu Mo Tu We Th Fr Sa\n", cal.week_row)
+		cal = Calendar.new(5, 2012)
+		assert_equal("      May 2012      \nSu Mo Tu We Th Fr Sa\n", cal.week_row)
 	end
 
 	def test_zeller_congruence
@@ -45,6 +45,31 @@ class CalTests < Test::Unit::TestCase
 		assert_equal(3, cal.zeller)
 	end
 
+	def test_number_of_days
+		cal = Calendar.new(6, 1900)
+		assert_equal(30, cal.days_number)
+	end
+
+	def test_number_of_days_february
+		cal = Calendar.new(2, 1995)
+		assert_equal(28, cal.days_number)
+	end
+
+	def test_number_of_days_leap_year
+		cal = Calendar.new(2, 1996)
+		assert_equal(29, cal.days_number)
+	end
+
+	def test_number_of_days_leap_century
+		cal = Calendar.new(2, 2000)
+		assert_equal(29, cal.days_number)
+	end
+
+	def test_number_of_days_non_leap_century
+		cal = Calendar.new(2, 1900)
+		assert_equal(28, cal.days_number)
+	end
+	
 	# def test_final_integration
 	# 	assert_equal `cal 2 2013`, `ruby cal.rb 2 2013`
 	# end
