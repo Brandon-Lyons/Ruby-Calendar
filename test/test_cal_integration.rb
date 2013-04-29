30f53b7f0e2e7face837e972c3c99e6214efcb61
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-04-29 11:41:15 -0500

    updated integration test

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -19,4 +19,12 @@ class CalTests < Test::Unit::TestCase
 	# 	assert_equal `cal 2 1900`, `ruby lib/cal.rb 2 1900`
 	# end
 
+	# def test_final_integration_more_than_usual_weeks
+	# 	assert_equal `cal 3 2013`, `ruby lib/cal.rb 3 2013`
+	# end
+
+	# def test_final_integration_less_than_usual_weeks
+	# 	assert_equal `cal 2 2009`, `ruby lib/cal.rb 2 2009`
+	# end
+
 end
