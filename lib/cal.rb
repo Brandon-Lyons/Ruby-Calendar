027fdb5e45867a3e3e7fd9e90a9584b5b133880e
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-05-01 12:32:26 -0500

    added ability to run if month is not an integer but a string instead

--- lib/cal.rb
+++ lib/cal.rb
@@ -1,7 +1,16 @@
 require_relative 'calendar'
+# require 'year'
 
-mon = ARGV[0]
-yr = ARGV[1]
+mon = String.new(ARGV[0])
+yr = String.new(ARGV[1])
 
 cal = Month.new(mon, yr)
 cal.display_single_month
+
+# if yr.nil? && mon.size >= 4
+# 	cal = Year.new(mon)
+# 	cal.display_year
+# else
+# 	cal = Month.new(mon, yr)
+# 	cal.display_single_month
+# end
--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,90 +1,96 @@
 class Month
-	attr_accessor :month
-	attr_accessor :year
-	attr_accessor :output
+  attr_accessor :month
+  attr_accessor :year
 
-	# month = ARGV[0]
-	# year = ARGV[1]
+  # month = ARGV[0]
+  # year = ARGV[1]
 
 
-	def initialize (month, year)
-		@month = month.to_i
-		@year = year.to_i
-		raise IndexError if month.to_i < 1 || month.to_i > 12 || year.to_i < 1800 || year.to_i > 3000
-	end
+  def initialize (month, year)
+    range = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+    if range.include? month
+      @month = month.to_i
+    else
+      months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+      month.to_s
+      month.capitalize!
+      @month = months.index(month).to_i + 1
+    end
+    @year = year.to_i
+    raise IndexError if @month.to_i < 1 || @month.to_i > 12 || @year.to_i < 1800 || @year.to_i > 3000
+  end
 
-	def header
-		month_name = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-		string = "#{month_name[@month - 1]} #{@year}".center(20).rstrip
-		string
-	end
+  def header
+    month_name = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    string = "#{month_name[@month - 1]} #{@year}".center(20).rstrip
+    string
+  end
 
-	def week_row
-		string = "Su Mo Tu We Th Fr Sa"
-	end
+  def week_row
+    string = "Su Mo Tu We Th Fr Sa"
+  end
 
-	def zeller
-		m = @month
-		y = @year
-		if @month == 1
-			m = 13
-			y = @year - 1
-		elsif @month == 2
-			m = 14
-			y = @year - 1
-		end
-		first_day = ((1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7) - 1
-		first_day = 6 if first_day == -1
-		first_day
-	end
+  def zeller
+    m = @month
+    y = @year
+    if @month == 1
+      m = 13
+      y = @year - 1
+    elsif @month == 2
+      m = 14
+      y = @year - 1
+    end
+    first_day = ((1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7) - 1
+    first_day = 6 if first_day == -1
+    first_day
+  end
 
-	def days_number
-		thirty = [4,6,9,11]
-		thirtyone = [1,3,5,7,8,10,12]
-		return 30 if thirty.include? @month
-		return 31 if thirtyone.include? @month
-		if @year % 100 == 0 && @year % 400 == 0
-			29
-		elsif @year % 4 == 0 && @year % 100 != 0
-			29
-		else
-			28
-		end					
-	end
+  def days_number
+    thirty = [4,6,9,11]
+    thirtyone = [1,3,5,7,8,10,12]
+    return 30 if thirty.include? @month
+    return 31 if thirtyone.include? @month
+    if @year % 100 == 0 && @year % 400 == 0
+      29
+    elsif @year % 4 == 0 && @year % 100 != 0
+      29
+    else
+      28
+    end
+  end
 
-	def format_range
-		range = (1..days_number).to_a
-		range.collect! do |num|
-			if num < 10
-				" " + num.to_s
-			else
-				num.to_s
-			end
-		end
-		range
-		dates = []
-		i = 0
-		until i == zeller
-			dates.unshift("  ")
-			i += 1
-		end
-		dates += range
-		dates
-	end
+  def format_range
+    range = (1..days_number).to_a
+    range.collect! do |num|
+      if num < 10
+        " " + num.to_s
+      else
+        num.to_s
+      end
+    end
+    dates = []
+    i = 0
+    until i == zeller
+      dates.unshift("  ")
+      i += 1
+    end
+    dates += range
+    dates
+  end
 
-	def display_single_month
-		string = header+ "\n" + week_row+ "\n"
-		dates = format_range
-		until dates.length == 0
-			add = dates.shift(7)
-			string << add.join(" ")
-			string << "\n"
-		end
-		until string.count("\n") == 7
-			string << "\n"
-		end
-		puts string
-	end
+  def display_single_month
+    string = header+ "\n" + week_row+ "\n"
+    dates = format_range
+    until dates.length == 0
+      add = dates.shift(7)
+      string << add.join(" ")
+      string << "\n"
+    end
+    until string.count("\n") == 7
+      string << "\n"
+    end
+    puts string
+  end
 
 end
 
